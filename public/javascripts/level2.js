12cb24d22a08e4bbc7024705f4ddeacfcd0e6fe2
Imran Ahmed Manzoor <imran.manzoor31@gmail.com>
2018-09-16 16:34:14 +0530

    All levels

--- public/javascripts/level2.js
+++ public/javascripts/level2.js
@@ -3,7 +3,7 @@ function submit() {
     $.post("https://damp-taiga-13917.herokuapp.com/level2", {code : c.value}, function(data) {
         if(data === "Yes") {
             alert("I firmly believe it.");
-            window.location.href= "https://damp-taiga-13917.herokuapp.com/level3";
+            window.location.href= "https://damp-taiga-13917.herokuapp.com/thelevel3";
         }
         else {
             alert("No.");
